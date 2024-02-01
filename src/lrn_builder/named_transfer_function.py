0187c684d14b4858b2b4fad181480228b3107d52
Joseph Hellerstein <jlheller@uw.edu>
2024-01-31 18:57:32 -0800

    NamedTransferFunction

--- src/lrn_builder/named_transfer_function.py
+++ src/lrn_builder/named_transfer_function.py
@@ -1,18 +1,24 @@
 '''Container for control.TransferFunction and common methods.'''
 
 import control # type: ignore
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from typing import Union, Optional, List
 import tellurium as te # type: ignore
 
 
+AX = "ax"
+FIGSIZE = "figsize"
+INPUT = "input"
+PREDICTION = "prediction"
+SIMULATION = "simulation"
 TIME = "time"
 TIMES = list(np.linspace(0, 100, 1000))
+TITLE = "title"
 
 
 class NamedTransferFunction(object):
-
     def __init__(self, input_name:str, output_name:str, transfer_function:control.TransferFunction):
         """
 
@@ -28,28 +34,29 @@ class NamedTransferFunction(object):
     def __repr__(self):
         return f"NamedTransferFunction({self.input_name}, {self.output_name}, {self.transfer_function})"
     
-    def _simulate(self, model:str, times:Optional[List]=TIMES, selections:Optional[List[str]]=None)->pd.DataFrame:
+    def _simulate(self, model:str, times:Optional[List]=TIMES)->pd.DataFrame:
         """
         Simulates the model using tellurium
 
         Args:
             model (str): Antimony model
             times (Optional[np.ndarray], optional): Simulation times
-            selections (Optional[List[str]], optional): Variables to select
 
         Returns:
-            pd.DataFrame
+            pd.DataFrame: columns
+                TIME, INPUT, SIMULATION
         """
         rr = te.loada(model)
-        if selections is None:
-            selections = [self.input_name, self.output_name]
-        if not TIME in selections:
-            selections = [TIME] + selections
+        selections = [TIME, self.input_name, self.output_name]
         rr.selections = selections
         data = rr.simulate(times[0], times[-1], len(times), selections=selections)   # type: ignore
         df = pd.DataFrame(data, columns=data.colnames)
         columns = [c[1:-1] if c[0] == '[' else c for c in df.columns]
-        df.columns = columns # type: ignore
+        df[INPUT] = df[self.input_name]
+        df[SIMULATION] = df[self.output_name]
+        for column in columns:
+            if not column in [TIME, INPUT, SIMULATION]:
+                del df[column]
         return df
 
     def predict(self, data:Union[pd.DataFrame, str], times:Optional[List]=TIMES)->pd.DataFrame:
@@ -60,32 +67,60 @@ class NamedTransferFunction(object):
             data (pd.DataFrame or antimony model): Dataframe Columns
                 TIME: times for predictions
                 <input_name>
+                <output_name> (simulated result)
+            times (Optional[List], optional): Times for predictions
 
         Returns:
             pd.DataFrame: Columns
                 TIME: times for predictions
                 <input_name>
-                <output_name>
+                SIMULATION: simulation of the output
+                PREDICTION
         """
         if isinstance(data, str):
-            df = self._simulate(data, times=times, selections=[self.input_name])
+            df = self._simulate(data, times=times)
         else:
             df = data
-        uvals = df[self.input_name].values
+        uvals = df[INPUT].values
         _, predictions = control.forced_response(self.transfer_function, T=df[TIME], U=uvals)
-        df[self.output_name] = predictions
+        df[PREDICTION] = predictions
         return df
     
-    def verify(self, data:Union[pd.DataFrame, str], is_plot:bool=False)->bool:
+    def verify(self, model:str, times:Optional[List]=TIMES, is_plot:bool=False, **kwargs)->bool:
         """
         Checks the predictions against the data.
 
-            data (pd.DataFrame or antimony model): Dataframe Columns
-                TIME: times for predictions
-                <input_name>
+        Args:
+            model (str): Antimony model
+            times (Optional[List], optional): Times for predictions
+            kwargs (dict): Additional arguments for plotting
 
         Returns:
             bool
         """
-        raise NotImplementedError("Must implement")
-        return False
+        df = self.predict(model, times=times)
+        simulations = df[SIMULATION].values
+        predictions = df[PREDICTION].values
+        # Check that the output is monotonic
+        rmse = np.sqrt(np.sum(simulations - predictions)**2)   # type: ignore
+        std = np.std(simulations)   # type: ignore
+        if is_plot:
+            if AX in kwargs.keys():
+                ax = kwargs[AX]
+            elif not FIGSIZE in kwargs.keys():
+                kwargs[FIGSIZE] = [5,5]
+                _, ax = plt.subplots(1, figsize=kwargs[FIGSIZE])
+            ax.scatter(simulations, predictions, color="red", marker="*")
+            ax.set_xlabel(SIMULATION)
+            ax.set_ylabel(PREDICTION)
+            max_simulated = np.max(simulations)   # type: ignore
+            max_predictions = np.max(predictions)  # type: ignore
+            max_value = max(max_simulated, max_predictions)
+            ax.plot([0, max_value], [0, max_value], linestyle="--")
+            if TITLE in kwargs.keys():
+                title = kwargs[TITLE]
+            else:
+                title = "%s->%s" % (self.input_name, self.output_name)
+            ax.set_title(title)
+            plt.show()
+        return rmse/std < 0.1
--- tests/test_named_transfer_function.py
+++ tests/test_named_transfer_function.py
@@ -1,16 +1,13 @@
 from lrn_builder.named_transfer_function import NamedTransferFunction # type: ignore
 
 import control # type: ignore
-import controlSBML as ctl # type: ignore
 import numpy as np
-import pandas as pd
-import re
 import unittest
-import tellurium as te # type: ignore
+import pandas as pd
 
 
-IGNORE_TEST = True
-IS_PLOT = True
+IGNORE_TEST = False
+IS_PLOT = False
 LINEAR_MDL = """
 model *main_model()
 species S1, S2
@@ -22,17 +19,7 @@ S1 = 10
 S2 = 0
 end
 """
-LINEAR_MDL1 = """
-model *main_model()
-species S1, S2
-S1 -> S2; k1*S1
-J2: S2 -> ; k2*S2
-k1 = 1
-k2 = 2
-S1 = 0
-S2 = 0
-end
-"""
+TRANFER_FUNCTION = control.TransferFunction([1], [1, 2])
 TIMES = list(np.linspace(0, 10, 100))
 
 
@@ -42,15 +29,40 @@ TIMES = list(np.linspace(0, 10, 100))
 class TestNamedTransferFunction(unittest.TestCase):
 
     def setUp(self):
+        self.ntf = NamedTransferFunction("S1", "S2", TRANFER_FUNCTION)
+
+    def testConstructor(self):
         if IGNORE_TEST:
             return
-        self.init()
+        self.assertEqual(self.ntf.input_name, "S1")
+        self.assertTrue(isinstance(self.ntf.transfer_function, control.TransferFunction))
+    
+    def testRepr(self):
+        if IGNORE_TEST:
+            return
+        self.assertTrue(isinstance(repr(self.ntf), str))
 
-    def init(self, model=LINEAR_MDL, times=TIMES):
-        k1 = 1
-        k2 = 2
-        tf = control.TransferFunction([k1], [1, k2])
-        self.network = SLMNetwork(model, "S1", "S2", k1, k2, tf, times=times)
+    def testSimulate(self):
+        if IGNORE_TEST:
+            return
+        df = self.ntf._simulate(LINEAR_MDL, TIMES)
+        diff = set(["time", "simulation", "input"]) - set(df.columns)
+        self.assertEqual(len(diff), 0)
+        self.assertTrue(isinstance(df, pd.DataFrame))
+
+    def testPredict(self):
+        if IGNORE_TEST:
+            return
+        df = self.ntf.predict(LINEAR_MDL, TIMES)
+        self.assertTrue(isinstance(df, pd.DataFrame))
+        diff = set(["time", "simulation", "input", "prediction"]) - set(df.columns)
+        self.assertEqual(len(diff), 0)
+
+    def testVerify(self):
+        if IGNORE_TEST:
+            return
+        result = self.ntf.verify(LINEAR_MDL, TIMES, is_plot=IS_PLOT)
+        self.assertTrue(result)
 
        
 
