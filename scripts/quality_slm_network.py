683f209aab4b15bc8ca47fa4a7c378f8e46733ba
Joseph Hellerstein <jlheller@uw.edu>
2024-02-21 14:11:47 -0800

    completing quality checks

--- scripts/quality_slm_network.py
+++ scripts/quality_slm_network.py
@@ -6,8 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-IGNORE_TEST = True
-IS_PLOT = False
+IS_DONT = False
 TIMES = np.linspace(0, 10, 100)
 NUM_STAGE = 3
 NUM_CHECK = 30
@@ -16,8 +15,9 @@ RESULT_DCT:dict = {}
 
 class QualityAnalyzer(object):
 
-    def __init__(self, num_stage=NUM_STAGE, times=TIMES):
+    def __init__(self, num_stage=NUM_STAGE, num_check=NUM_CHECK, times=TIMES):
         self.num_stage = num_stage
+        self.num_check = num_check
         self.times = times
         self.network = self.makeNetwork()
 
@@ -32,15 +32,19 @@ class QualityAnalyzer(object):
         for method in methods:
             statement = "self.%s()" % method
             result_dct[method] = eval(statement)
-        nbin = int(NUM_CHECK/5)
+        nbin = max(int(self.num_check/5), 2)
+        nbin = 100
         bins = np.linspace(0, 1, nbin)
         for method in result_dct.keys():
+            if len(result_dct[method]) == 0:
+                continue
             _, ax = plt.subplots(1)
             ax.hist(result_dct[method], bins=bins, label=method)
             ax.set_xlim([0, 1])
             ax.set_ylim([0, len(result_dct[method])])
             ax.set_xlabel("score")
             ax.set_ylabel("count")
+            ax.set_title(method[2:])
         plt.show()
 
     def doConcatenate(self):
@@ -48,26 +52,74 @@ class QualityAnalyzer(object):
         Tests concatenate of networks
         """
         results = []
-        for _ in range(NUM_CHECK):
+        if IS_DONT:
+            return results
+        for _ in range(self.num_check):
             network = self.makeNetwork()
             cnetwork = self.network.concatenate(network)
             _, builder = cnetwork.plotStaircaseResponse(is_plot=False, times=TIMES)
             _, score = cnetwork.named_transfer_function.evaluate(str(builder), is_plot=False,
                                                                  fractional_deviation=FRACTIONAL_DEVIATION)
-            plt.close()
             results.append(score)
         return results
 
-    def testBranchjoin(self):
-        if IGNORE_TEST:
-            return
+    def doBranchjoin(self):
         results = []
-        for _ in range(NUM_CHECK):
+        if IS_DONT:
+            return results
+        for _ in range(self.num_check):
             network = self.makeNetwork()
-            bjn = self.network.branchjoin(network)
-            results.append(cnetwork.isValid(is_plot=IS_PLOT, fractional_deviation=FRACTIONAL_DEVIATION, times=TIMES))
-        print(sum(results)/len(results))
-        self.assertTrue(all(results))
+            k1a, k1b, k2a, k2b, k3 = np.random.uniform(0, 10, 5)
+            bjn = self.network.branchjoin(network, k1a=k1a, k1b=k1b, k2a=k2a, k2b=k2b, k3=k3)
+            _, builder = bjn.plotStaircaseResponse(is_plot=False, times=TIMES)
+            df, score = bjn.named_transfer_function.evaluate(str(builder), is_plot=False,
+                                                                 fractional_deviation=FRACTIONAL_DEVIATION)
+            results.append(score)
+        return results
+    
+    def doScale(self):
+        results = []
+        if IS_DONT:
+            return results
+        for _ in range(self.num_check):
+            network = self.makeNetwork()
+            k1, k2, scale = np.random.uniform(0, 10, 3)
+            network = self.network.scale(m=scale, k1=k1, k2=k2)
+            _, builder = network.plotStaircaseResponse(is_plot=False, times=TIMES)
+            df, score = network.named_transfer_function.evaluate(str(builder), is_plot=False,
+                                                                 fractional_deviation=FRACTIONAL_DEVIATION)
+            results.append(score)
+        return results
+    
+    def doPfeedback(self):
+        results = []
+        if IS_DONT:
+            return results
+        for _ in range(self.num_check):
+            network = self.makeNetwork()
+            k1, k2, k3, k4, k5 = np.random.uniform(0, 100, 5)
+            network = self.network.pfeedback(k1=k1, k2=k2, k3=k3, k4=k4, k5=k5)
+            _, builder = network.plotStaircaseResponse(is_plot=False, times=TIMES)
+            df, score = network.named_transfer_function.evaluate(str(builder), is_plot=False,
+                                                                 fractional_deviation=FRACTIONAL_DEVIATION)
+            import pdb; pdb.set_trace()
+            results.append(score)
+        return results
+    
+    def doNfeedback(self):
+        results = []
+        if IS_DONT:
+            return results
+        for _ in range(self.num_check):
+            network = self.makeNetwork()
+            k1, k2, k3, k4, k5 = np.random.uniform(0, 100, 5)
+            network = self.network.nfeedback(k1=k1, k2=k2, k3=k3, k4=k4, k5=k5)
+            _, builder = network.plotStaircaseResponse(is_plot=False, times=TIMES)
+            df, score = network.named_transfer_function.evaluate(str(builder), is_plot=False,
+                                                                 fractional_deviation=FRACTIONAL_DEVIATION)
+            import pdb; pdb.set_trace()
+            results.append(score)
+        return results
        
 
 if __name__ == '__main__':
