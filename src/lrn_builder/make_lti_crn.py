Fix bug in calculation of transfer functions

src/lrn_builder/make_lti_crn.py
@@ -3,14 +3,16 @@
 """
 Characteristics of the generated CRN.
 1. Behaves as a linear system of ODEs.
-2. Each reaction has one reactant and one or more products.
+2. Each reaction has zero or one reactant and zero or more products.
 3. Kinetic constants are randomly assigned within specified bounds.
 4. Stoichiometry of products is randomly assigned within specified bounds.
 5. The first reaction has no reactant (i.e., it is a source reaction).
 6. Subsequent reactions select reactants from existing species to ensure connectivity.
-7. Species are named sequentially (S1, S2, S3, ...).
+7. Species are named sequentially (S1_, S2_, S3_, ...).
 8. Kinetic constants are named sequentially (k1, k2, k3,
-9. Species S1 is an input boundary.
+9. Species S1 is an input boundary with the kinetic constant k1 = 1.
+10. The system is assured to be stable by adding degradation reactions if necessary.
+11. The system is assured to be non-trivial by ensuring that S1 is a reactant in at least one reaction.
 """
 
 import random
@@ -34,6 +36,7 @@ def makeLtiCrn(
     """
     Generate a random linear time-invariant (LTI) chemical reaction network
     in Antimony language.
+    S1 is the boundary species and its initial value is 1.
     
     Parameters:
     -----------
@@ -85,7 +88,7 @@ def makeLtiCrn(
     # Track existing species and rate constants
     rate_constants = []
     
-    # Reaction 1: No reactant, S1 is product
+    """ # S1 is a boundary species and k1 is its initial value
     k1 = np.random.uniform(kinetic_constant_bounds[0], kinetic_constant_bounds[1])
     rate_constants.append((f"{rate_constant_prefix}1", k1))
     if is_input_boundary:
@@ -93,6 +96,8 @@ def makeLtiCrn(
     else:
         prefix = ""
     antimony_lines.append(f"  -> {prefix}{input_species_name}; {rate_constant_prefix}1")
+    """
+    antimony_lines.append(f"  species {input_species_name};\n")
     existing_species = [makeSpeciesName(n) for n in range(1, num_species + 1)]
     candidate_product_species = existing_species[2:]
     
@@ -142,7 +147,11 @@ def makeLtiCrn(
     antimony_lines.append("\n  # Species initialization")
     for species in sorted(existing_species, key=lambda x: extractSpeciesNumber(x)):
         if species == input_species_name:
-            antimony_lines.append(f"  {species} = 1  # Input boundary species")
+            if is_input_boundary:
+                prefix = "$"
+            else:
+                prefix = ""
+            antimony_lines.append(f"  {prefix}{species} = 1  # Input boundary species")
         else:
             antimony_lines.append(f"  {species} = 0")
     
@@ -184,5 +193,5 @@ def makeLtiCrn(
     if np.any(eigenvalues.real > 0):
         import pdb; pdb.set_trace()
         pass
-    
+
     return antimony_str

src/lrn_builder/siso_analyzer.py
@@ -10,6 +10,7 @@ Conventions for Antimony models.
 from src.lrn_builder.symbolic_jacobian_maker import SymbolicJacobianMaker
 
 from collections import namedtuple
+import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
 import pandas as pd # type: ignore
 from scipy import signal  # type: ignore
@@ -86,6 +87,7 @@ class SISOAnalyzer(object):
                             columns=sorted_species_names)
         # Other attributes
         self.jacobian_smat = maker.jacobian_smat
+        self.b_smat = maker.b_smat
         self.kinetic_constant_dct = maker.kinetic_constant_dct
         self.species_names = maker.species_names
         self.num_species = len(self.species_names)
@@ -109,6 +111,7 @@ class SISOAnalyzer(object):
         else:
             output_name = proposed_output_name
         if output_name not in candidate_names:
+            import pdb; pdb.set_trace()
             raise ValueError(f"Output species {self.output_name} not found in model species: {candidate_names}")
         #
         return output_name
@@ -121,10 +124,15 @@ class SISOAnalyzer(object):
             # Construct the transfer function expression
             input_index = self.species_names.index(self.input_name)
             s = sp.Symbol("s")
-            B = sp.Matrix(np.zeros((self.num_species, self.num_species)))
-            B[input_index, input_index] = 1
+            B = sp.eye(self.jacobian_smat.rows)
             I = sp.eye(self.jacobian_smat.rows)
-            self._transfer_function_smat = (s*I - self.jacobian_smat).inv() * B
+            A_smat = self.jacobian_smat
+            b_smat = sp.Matrix.zeros(self.jacobian_smat.rows, 1)
+            b_smat[input_index] = 1
+            b_smat += self.b_smat
+            A_smat[input_index, :] = sp.Matrix.zeros(1, self.jacobian_smat.cols)
+            # Multiply the step response by s to get the transfer function
+            self._transfer_function_smat = sp.simplify(s*((s*I - A_smat).inv() * B * b_smat))
         return self._transfer_function_smat
     
     @property
@@ -219,18 +227,17 @@ class SISOAnalyzer(object):
         """
         kinetic_constant_dct = dict(self.kinetic_constant_dct)
         kinetic_constant_dct.update(kwargs)
-        transfer_function_expr = self.transfer_function_expr.subs(kinetic_constant_dct)
+        # Substitute kinetic constants into symbolic transfer function expression
+        transfer_function_smat = sp.simplify(self.transfer_function_expr.subs(kinetic_constant_dct))
         # All free symbols except for s should be removed. Now convert to transfer function
-        free_syms = transfer_function_expr.free_symbols
+        free_syms = transfer_function_smat.free_symbols
         if len(free_syms) == 0:
-            return signal.TransferFunction([float(transfer_function_expr)], [1])
+            return signal.TransferFunction([float(transfer_function_smat)], [1])
         elif len(free_syms) > 1:
             raise ValueError(f"Expression contains multiple symbols: {free_syms}. "
                             "Only single-variable rational functions are supported.")
         s = free_syms.pop()
-        # Simplify and get numerator and denominator
-        transfer_function_expr = sp.simplify(transfer_function_expr)
-        numer, denom = sp.fraction(transfer_function_expr)
+        numer, denom = sp.fraction(transfer_function_smat)
         # Convert to polynomials
         numer_poly = sp.Poly(numer, s)
         denom_poly = sp.Poly(denom, s)
@@ -241,3 +248,27 @@ class SISOAnalyzer(object):
         self.transfer_function = signal.TransferFunction(numer_coeffs, denom_coeffs) 
         #
         return self.transfer_function
+
+    def plotTransferFunctionValidation(self, step_size: float = 1.0) -> None:
+        """
+        Plots the simulated and transfer function predicted outputs.
+
+        Args:
+            step_size (float): The magnitude of the step input.
+        """
+        tf = self.makeTransferFunction()
+        rr = te.loada(self.original_antimony_str)
+        data = rr.simulate(0, 10, 100)
+        times = data[:, 0]
+        _, predicted_y = signal.step(tf, T=times)
+        true_y = data[f"[{self.output_name}]"]
+        # Plot the step response comparison
+        plt.figure()
+        plt.scatter(true_y, predicted_y*step_size)
+        plt.plot([0, max(true_y)], [0, max(true_y)], 'r--', label="Ideal")
+        plt.title(f"Predicted vs. Actual Step Response for {self.output_name}")
+        plt.xlabel("Actual")
+        plt.ylabel("Predicted")
+        plt.grid()
+        plt.legend(["Comparison", "Ideal"])
+        plt.show()